a6847b764d0b414dfb62ac33bf1877a2f4a534ac
Paddywan <paddy@bitnibbler.co.uk>
2019-05-27 08:07:18 +0100

    Add project files.

---
+++ BossDropRemoval/BossDropRemoval.cs
@@ -0,0 +1,34 @@
+﻿using RoR2;
+using BepInEx;
+using System.Reflection;
+using R2API;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using BepInEx.Configuration;
+using System.Reflection;
+using MonoMod.Cil;
+using KinematicCharacterController;
+using System.Linq;
+//using EntityStates;
+
+
+
+namespace Paddywan
+{
+    [BepInDependency("com.bepis.r2api")]
+
+    [BepInPlugin("com.Paddywan.BossDropRemoval", "BossDropRemoval", "1.0.0")]
+
+    public class BossDropRemoval : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.BossGroup.OnCharacterDeathCallback += (orig, self, damageReport) =>
+            {
+                self.bossDropChance = 0.0f;
+                orig(self, damageReport);
+            };
+        }
+    }
+}
